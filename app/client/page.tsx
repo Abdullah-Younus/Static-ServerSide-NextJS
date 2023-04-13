d47e4d78f8541d883c20c7c4cd5d63668995233a
Abdullah-Youns <126175476+Abdullah-Youns@users.noreply.github.com>
2023-04-13 17:41:38 +0500

    server and client fetching data

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,24 +1,24 @@
-"use client"
+"use client";
 import React from 'react';
+import useSWR from 'swr';
 
-type Book = {
-    id: number;
-    name: string;
-    type: string;
-    available: boolean;
-}
 
-async function getBooks() {
-    const books = await fetch('https://simple-books-api.glitch.me/books');
-    const data = books.json();
-    return data;
-}
 
-async function page() {
-    const books = await getBooks();
+const url = "https://api.quotable.io/random?tags=technology";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+function page() {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const { data, error, isLoading } = useSWR(url, fetcher);
+    if (error) return <div>Error</div>
+    if (isLoading) return <div>Loading...........</div>
 
     return (
-        <div>Client</div>
+        <div>
+            <h1>Client</h1>
+            {data.content}
+        </div>
     )
 }
 
